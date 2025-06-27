a0eb02d0a58953e5e97cb951bed5bb2735227faf
golti-saikiran <saikiran.golti@gmail.com>
2025-06-27 17:25:24 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -20,14 +20,13 @@ const allowedOrigins = [
 console.log('Allowed Origins:', allowedOrigins);
 app.use('/uploads', express.static('uploads'));
 app.use(cors({
-  // origin: function (origin, callback) {
-  //   if (!origin || allowedOrigins.includes(origin)) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'));
-  //   }
-  // },
-  origin:true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(bodyParser.json());
