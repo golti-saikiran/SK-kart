80a697374c3de665737269b3c17b1fc47e563861
golti-saikiran <saikiran.golti@gmail.com>
2025-06-27 17:45:11 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -17,10 +17,10 @@ const allowedOrigins = [
   process.env.FRONTEND_URL_DEV,
   process.env.FRONTEND_URL_PROD,
 ];
-console.log('Allowed Origins:', allowedOrigins);
 app.use('/uploads', express.static('uploads'));
 app.use(cors({
   origin: function (origin, callback) {
+    console.log('CORS Origin:', origin);
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
